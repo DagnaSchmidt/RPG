7999e22c19bfc4b0ac3ea8e21a55b9f828bd641f
Dagna Schmidt <dagdagi889@gmail.com>
2023-01-08 13:38:33 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Welcome from './Components/Welcome';
+import LogIn from './Components/LogIn';
+import SignIn from './Components/SignIn';
+import NewAdventure from './Components/NewAdventure';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Welcome />} />
+          <Route path='/login' element={<LogIn />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/newadventure' element={<NewAdventure />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/NewAdventure.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewAdventure = () => {
+  return (
+    <div>NewAdventure</div>
+  )
+}
+
+export default NewAdventure;
