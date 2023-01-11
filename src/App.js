71f7ab98ef751ad5f06cad2be8dad3dc23d36577
anumol.abraham <anumol.va@outlook.com>
2023-01-11 13:58:08 +0100

    Battle component initial code

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import UserMenu from "./Components/UserMenu";
 import CharacterSelection from "./Components/CharacterSelection";
 import "./styles/CharacterSelection.css";
 import "./styles/Welcome.css";
+import "./styles/Battle.css";
+import Battle from "./Components/Battle";
 
 function App() {
   const [name, setName] = useState("");
@@ -51,6 +53,7 @@ function App() {
           <Route path='/newadventure' element={<NewAdventure name={name} characterName={characterName} setCharacterName={setCharacterName} characterLifePoints={characterLifePoints} setCharacterLifePoints={setCharacterLifePoints} characterImage={characterImage} setCharacterImage={setCharacterImage} opponentName={opponentName} setOpponentName={setOpponentName} opponentLifePoints={opponentLifePoints} setOpponentLifePoints={setOpponentLifePoints} opponentImage={opponentImage} setOpponentImage={setOpponentImage} />} />
           <Route path='/usermenu' element={<UserMenu logOut={logOut}/>} />
           <Route path='/characterselection' element={<CharacterSelection />} />
+          <Route path='/battle' element={<Battle name={name} characterName={characterName} setCharacterName={setCharacterName} characterLifePoints={characterLifePoints} setCharacterLifePoints={setCharacterLifePoints} characterImage={characterImage} setCharacterImage={setCharacterImage} opponentName={opponentName} setOpponentName={setOpponentName} opponentLifePoints={opponentLifePoints} setOpponentLifePoints={setOpponentLifePoints} opponentImage={opponentImage} setOpponentImage={setOpponentImage} />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/Battle.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import Button from "react-bootstrap/Button";
+import pokemonImg from "../assets/images/pokemon.gif";
+
+const Battle = ({ characterName, characterLifePoints, characterImage, opponentName, opponentLifePoints, opponentImage }) => {
+    return (
+        <section >
+            <h1 className='battle-heading'>Pokemon Battle</h1>
+            <div className='container-battle'>
+                <div className='container-battle-child'>
+                    <div>User Name: {characterName}</div>
+                    <div>User Life points: {characterLifePoints}</div>
+                    <img src={pokemonImg} alt="pokemon pic" />
+                </div>
+                <div className='container-battle-child'>
+                    <div>Opponent Name: {opponentName}</div>
+                    <div>Opponent Life points: {opponentLifePoints}</div>
+                    <img src={pokemonImg} alt="pokemon pic" />
+                </div>
+            </div>
+
+            <div className='battle-btns'>
+                <Button className='battle-btn'>
+                    <Link className='btn-link'>Fight</Link>
+                </Button>
+                <Button className='battle-btn'>
+                    <Link className='btn-link'>Heal</Link>
+                </Button>
+            </div>
+        </section>
+    );
+};
+
+export default Battle;
